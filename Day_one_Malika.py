f76232590124a3132da12b00287f7a6e0b21d964
malikanatbek <malikanatbek@gmail.com>
2020-03-26 15:29:37 +0600

    Add Day_one_Malika.py

---
+++ Day_one_Malika.py
@@ -0,0 +1,38 @@
+Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Choco=75
+>>> Coffee=245
+>>> Milk=70
+>>> Choco+Coffee+Milk
+390
+>>> var_int=10
+>>> var_float=8.4
+>>> var_str="No"
+>>> var_big=var_int*3.5
+>>> var_float=var_float-1
+>>> var_int/var_float
+1.3513513513513513
+>>> var_big/var_float
+4.72972972972973
+>>> var_str=var_str*2+"Yes"*3
+>>> var_int
+10
+>>> var_float
+7.4
+>>> var_str
+'NoNoYesYesYes'
+>>> var_big
+35.0
+>>> a=67
+>>> b=21
+>>> c=a//b
+>>> d=a%b
+>>> e=a**b
+>>> c
+3
+>>> d
+4
+>>> e
+222623423334106740048017938136372833267
+>>> 
